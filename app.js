5c40154a7c23619cd500078082d59fd114c39ecd
Saurav s Mehta <sauravmehta631@gmail.com>
2025-12-06 14:08:44 +0530

    show+index route created

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
 
 let Port = 8080;
 const MONGO_URL = "mongodb://127.0.0.1:27017/chillcasa";
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 main().then((res)=>{
     console.log("MongoDB connetion successful");
@@ -18,18 +25,25 @@ app.get("/",(req,res)=>{
     res.send("At Home");
 })
 
-app.get("/testListing",async(req,res)=>{
-    let sampleListing = new Listing({
-        title : "my new Villa",
-        description : "by the beach",
-        price : 3999,
-        location : "Mubai",
-        country: "India"
-    });
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful");
+//create route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
 })
+
+//index route
+app.get("/listings",async (req,res)=>{
+   const allListings = await Listing.find();
+   res.render("listings/index.ejs",{allListings});
+})
+
+//show route
+app.get("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show",{listing});
+})
+
+
 
 app.listen(Port,()=>{
     console.log("listening to the Port : 8080");
