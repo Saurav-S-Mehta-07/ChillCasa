e3a47f3e96d588cbc41ea7a2bd8953bfcb4cbd82
Saurav s Mehta <sauravmehta631@gmail.com>
2025-12-08 17:06:45 +0530

    server-side error handling added

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require('./utils/wrapAsync.js');
+const ExpressError = require("./utils/ExpressError.js");
+const { wrap } = require("module");
 
 let Port = 8080;
 const MONGO_URL = "mongodb://127.0.0.1:27017/chillcasa";
@@ -30,10 +32,10 @@ app.get("/",(req,res)=>{
 })
 
 //index route
-app.get("/listings",async (req,res)=>{
+app.get("/listings",wrapAsync(async (req,res)=>{
    const allListings = await Listing.find();
    res.render("listings/index.ejs",{allListings});
-})
+}));
 
 //create route
 app.get("/listings/new",(req,res)=>{
@@ -41,44 +43,54 @@ app.get("/listings/new",(req,res)=>{
 })
 
 app.post("/listings",wrapAsync(async(req,res,next)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"send valid data for listing");
+    }
      const newListing  = new Listing(req.body.listing);
      await newListing.save();
      res.redirect("/listings");
 }));
 
 //update route
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",wrapAsync(async(req,res)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"send valid data for listing");
+    }
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
-})
+}));
 
 //show route
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show",{listing});
-})
+}));
 
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     let listing = await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
-})
+}));
 
 //delete route
-app.delete("/listings/:id", async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
    let {id} = req.params;
    await Listing.findByIdAndDelete(id);
    res.redirect("/listings");
-})
+}));
 
+//page not found
+app.use((req,res,next)=>{
+    next(new ExpressError(404,"Page not found"));
+});
 
 //error handling middleware
 app.use((err,req,res,next)=>{
-   res.send("Something wet wrong");
+   let {status=500, message="something went wrong"} = err;
+   res.status(status).send(message);
 })
-
 
 app.listen(Port,()=>{
     console.log("listening to the Port : 8080");
